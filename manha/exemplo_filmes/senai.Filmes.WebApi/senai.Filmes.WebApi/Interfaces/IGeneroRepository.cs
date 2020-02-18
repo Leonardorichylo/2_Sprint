1549171798bf984aade22b0d1725524351c64c5d
Leonardorichylo <leonardorichy@outlook.com.br>
2020-02-18 10:05:15 -0300

    18/02

---
+++ manha/exemplo_filmes/senai.Filmes.WebApi/senai.Filmes.WebApi/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,46 @@
+﻿using senai.Filmes.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.Filmes.WebApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório Genero
+    /// </summary>
+    interface IGeneroRepository
+    {
+        /// <summary>
+        /// Lista todos os gêneros
+        /// </summary>
+        /// <returns>Retorna uma lista de gêneros</returns>
+        List<GeneroDomain> Listar();
+        /// <summary>
+        /// Cadastra um novo genero
+        /// </summary>
+        /// <param name="genero">Objeto genero que sera cadastrado</param>
+        void Cadastrar(GeneroDomain genero);
+
+        /// <summary>
+        /// Deletar Genero
+        /// </summary>
+        /// <param name="id"></param>
+        void Deletar(int id);
+
+        /// <summary>
+        /// Atualiza um genero existente
+        /// </summary>
+        /// <param name="genero">Objeto genero sera atualizado</param>
+        void AtualizarIdCorpo(GeneroDomain genero);
+
+        /// <summary>
+        /// Atualiza um genero existente
+        /// </summary>
+        /// <param name="id">ID do genero que sera</param>
+        /// <param name="genero"></param>
+        void AtualizarIdUrl(int id, GeneroDomain genero);
+
+       
+    }
+}
